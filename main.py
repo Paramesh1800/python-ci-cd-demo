8afd4d7ff55d7328c932bc740515fb4b150cfb09
Paramesh1800 <sparamesh830@gmail.com>
2026-06-16 16:17:00 +0530

    Update application

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition:", add(10, 5))
+    print("Subtraction:", sub(10, 5))
